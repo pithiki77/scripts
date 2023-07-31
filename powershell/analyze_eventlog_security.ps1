821f5cb049d38324d07e08044b38bc12d5c691ba
pithiki77 <panos.ef@gmail.com>
2023-07-31 13:03:48 +0300

    aa

--- powershell/analyze_eventlog_security.ps1
+++ powershell/analyze_eventlog_security.ps1
@@ -1,4 +1,4 @@
-$d = Get-EventLog -Log Security | Where-Object {$_.InstanceID -eq 5152}
+$d = Get-EventLog -Log Security -Newest 200 | Where-Object {$_.InstanceID -eq 5152}
 
 ## Now the variable $d contains all of the events for dropped packets.  Doing a $d | gm will show the lack of options.  So we will use mjlinor’s secret sauce to parse the message fields and make those important fields a property of the object.
 
@@ -12,7 +12,8 @@ $d | ForEach-Object {$_.message -match "Destination Port:\s+(\S+)">$null;$_ | Ad
 
 ## Now running a $d | gm should show the additional properties of this object.  And running the following command will actually give us readable information!
 
-##   $d | ft TimeGenerated, SrcIP, SrcPort, Direction, DstIP, DstPort, Protocol -autosize
+##  $d | ft TimeGenerated, SrcIP, SrcPort, Direction, DstIP, DstPort, Protocol -autosize
 
-$d | Where-Object -Property SrcPort -EQ 443| ft TimeGenerated, SrcIP, SrcPort, Direction, DstIP, DstPort, Protocol -autosize
+## $d | Where-Object -Property SrcPort -EQ 443| ft TimeGenerated, SrcIP, SrcPort, Direction, DstIP, DstPort, Protocol -autosize
 
+$d | Where-Object -Property SrcIP -NE 10.11.12.18| Where-Object -Property SrcIP -NE 10.11.12.34|Where-Object -Property SrcIP -NE 192.168.1.142|Where-Object -Property SrcIP -NE 192.168.0.201| ft TimeGenerated, SrcIP, SrcPort, Direction, DstIP, DstPort, Protocol -autosize
