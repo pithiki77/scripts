77493b28e53087e8922d9061accea2b6b1fa22e4
pithiki77 <panos.ef@gmail.com>
2023-12-04 09:00:04 +0200

    a

--- powershell/serveracademy/part28/test/part28.ps1
+++ powershell/serveracademy/part28/test/part28.ps1
@@ -1,3 +1,5 @@
 #requires -RunAsAdministrator
 
 import-csv -path .\powershell\serveracademy\part28\test\aliases.csv | New-Alias -verbose
+
+import-csv -path .\powershell\serveracademy\part28\test\bits.csv | Stop-Service -PassThru
