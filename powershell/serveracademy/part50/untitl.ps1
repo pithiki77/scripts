b198a79f2822e9e26d4892ea66fc0f426d22c967
pithiki77 <panos.ef@gmail.com>
2023-12-19 09:15:45 +0200

    a

--- powershell/serveracademy/part50/untitl.ps1
+++ powershell/serveracademy/part50/untitl.ps1
@@ -1,1 +1,9 @@
 echo "test"
+
+Get-ExecutionPolicy
+Get-ExecutionPolicy -List
+Set-ExecutionPolicy -Scope CurrentUser Default
+c:\Users\panos\git\scripts\powershell\serveracademy\part50\untitl.ps1
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+c:\Users\panos\git\scripts\powershell\serveracademy\part50\untitl.ps1
+Get-ExecutionPolicy
