d58b458a49e43deb7d16a991a3688b7680b52a26
pithiki77 <panos.ef@gmail.com>
2024-01-12 09:19:40 +0200

    a

--- windows fixes/winrefix_part1.ps1
+++ windows fixes/winrefix_part1.ps1
@@ -5,7 +5,7 @@ $nfo -match “.*Windows RE location.*harddisk(\d+)” | Out-Null #Locate the di
 $disk = $Matches[1]
 $nfo -match “.*Windows RE location.*partition(\d+)” | Out-Null #Locate the partition it is on.
 $partition = $Matches[1]
-New-Object -TypeName psobject -Property $([ordered]@{Enabled=’True’;Disk=$disk;Partition=$partition;Resizable=(((Get-Disk -Number $disk | Get-Partition).PartitionNumber | Measure-Object -Maximum).Maximum -eq $partition);CurrentSize=([string]((Get-Disk -Number $disk | Get-Partition | Where-Object PartitionNumber -eq $partition).Size / 1MB) +’MB’);A1_Key=[System.GUID]::NewGuid()})
+New-Object -TypeName psobject -Property $([ordered]@{Enabled='True';Disk=$disk;Partition=$partition;Resizable=(((Get-Disk -Number $disk | Get-Partition).PartitionNumber | Measure-Object -Maximum).Maximum -eq $partition);CurrentSize=([string]((Get-Disk -Number $disk | Get-Partition | Where-Object PartitionNumber -eq $partition).Size / 1MB) +'MB');A1_Key=[System.GUID]::NewGuid()})
 }else{
-New-Object -TypeName psobject -Property $([ordered]@{Enabled=’False’;Disk=’N/A’;Partition=’N/A’;Resizable=’N/A’;CurrentSize=’N/A’;A1_Key=[System.GUID]::NewGuid()})
+New-Object -TypeName psobject -Property $([ordered]@{Enabled='False';Disk='N/A';Partition='N/A';Resizable='N/A';CurrentSize='N/A';A1_Key=[System.GUID]::NewGuid()})
 }
