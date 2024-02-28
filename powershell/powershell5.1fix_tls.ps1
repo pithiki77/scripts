80e0da5e3d8e634ec8a97d6cb7947ef8a7d787c1
pithiki77 <panos.ef@gmail.com>
2024-02-28 20:24:36 +0200

    aa

--- powershell/powershell5.1fix_tls.ps1
+++ powershell/powershell5.1fix_tls.ps1
@@ -10,6 +10,20 @@
 
 
 
+#================================================
+    $ProfileFile = "${PsHome}\Profile.ps1"
+
+if (! (Test-Path $ProfileFile)) {
+New-Item -Path $ProfileFile -Type file -Force
+}
+''                                                                                | Out-File -FilePath $ProfileFile -Encoding ascii -Append
+'# It is 2018, SSL3 and TLS 1.0 are no good anymore'                              | Out-File -FilePath $ProfileFile -Encoding ascii -Append
+'[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12' | Out-File -FilePath $ProfileFile -Encoding ascii -Append
+
+notepad $ProfileFile
+
+#===================================================
+
 set-PSRepository -Name psgallery -InstallationPolicy Trusted
 
 install-module psreadline
